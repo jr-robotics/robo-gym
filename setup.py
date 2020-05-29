caf8a6e9f1e56577723bf299526b50e3a6a6632a
Matteo Lucchi <matteo.lucchi@joanneum.at>
2020-05-29 16:18:44 +0200

    added automatic deploy to PYPI when tagging version

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import setuptools
+import sys, os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'robo_gym'))
+from version import VERSION
 
 setuptools.setup(name='robo-gym',
-      version='0.1.1',
+      version=VERSION,
       description='robo-gym: an open source toolkit for Distributed Deep Reinforcement Learning on real and simulated robots.',
       url='https://github.com/jr-robotics/robo-gym',
       author="Matteo Lucchi, Friedemann Zindler",
