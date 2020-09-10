b96aa333023155a8e11a51a717ff604dd65a05fe
Friedemann Zindler <friedemann.zindler@joanneum.at>
2020-09-10 14:48:18 +0200

    UR environemnt changes and refactor

--- robo_gym/envs/__init__.py
+++ robo_gym/envs/__init__.py
@@ -5,5 +5,5 @@ from robo_gym.envs.mir100.mir100 import ObstacleAvoidanceMir100Rob
 
 from robo_gym.envs.ur10.ur10 import EndEffectorPositioningUR10Sim
 from robo_gym.envs.ur10.ur10 import EndEffectorPositioningUR10Rob
-from robo_gym.envs.ur10.ur10 import EndEffectorPositioningAntiShakeUR10Sim
-from robo_gym.envs.ur10.ur10 import EndEffectorPositioningAntiShakeUR10Rob
+from robo_gym.envs.ur10.ur10 import EndEffectorPositioningUR10DoF5Sim
+from robo_gym.envs.ur10.ur10 import EndEffectorPositioningUR10DoF5Rob
--- robo_gym/envs/ur10/ur10.py
+++ robo_gym/envs/ur10/ur10.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys, math, copy, random
+from matplotlib.pyplot import step
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 import gym
@@ -29,8 +30,7 @@ class UR10Env(gym.Env):
     """
     real_robot = False
 
-    def __init__(self, rs_address=None, max_episode_steps=500, **kwargs):
-
+    def __init__(self, rs_address=None, max_episode_steps=300, **kwargs):
         self.ur10 = ur_utils.UR10()
         self.max_episode_steps = max_episode_steps
         self.elapsed_steps = 0
@@ -105,6 +105,12 @@ class UR10Env(gym.Env):
         # Check if the environment state is contained in the observation space
         if not self.observation_space.contains(self.state):
             raise InvalidStateError()
+        
+        _, _, done, _ = self.step(self.state[3:8])
+        self.elapsed_steps = 0
+        if done:
+            raise InvalidStateError()
+            
 
         return self.state
 
@@ -119,6 +125,7 @@ class UR10Env(gym.Env):
 
         # Convert environment action to Robot Server action
         rs_action = copy.deepcopy(action)
+
         # Scale action
         rs_action = np.multiply(rs_action, self.abs_joint_pos_range)
         # Convert action indexing from ur10 to ros
@@ -208,7 +215,6 @@ class UR10Env(gym.Env):
             np.array: [x,y,z,alpha,theta,gamma] pose.
 
         """
-
         return self.ur10.get_random_workspace_pose()
 
     def _robot_server_state_to_env_state(self, rs_state):
@@ -236,8 +242,6 @@ class UR10Env(gym.Env):
         base_to_ee_translation = - ee_to_base_translation
 
         target_coord_ee_frame = utils.change_reference_frame(target_coord,base_to_ee_translation,base_to_ee_quaternion)
-        print('Target coords in EE frame')
-        print(target_coord_ee_frame)
         target_polar = utils.cartesian_to_polar_3d(target_coord_ee_frame)
 
         # Transform joint positions and joint velocities from ROS indexing to
@@ -245,8 +249,11 @@ class UR10Env(gym.Env):
         ur_j_pos = self.ur10._ros_joint_list_to_ur10_joint_list(rs_state[6:12])
         ur_j_vel = self.ur10._ros_joint_list_to_ur10_joint_list(rs_state[12:18])
 
+        # Normalize joint position values
+        ur_j_pos_norm = self.ur10.normalize_joint_values(joints=ur_j_pos)
+
         # Compose environment state
-        state = np.concatenate((target_polar,ur_j_pos, ur_j_vel))
+        state = np.concatenate((target_polar, ur_j_pos_norm, ur_j_vel))
 
         return state
 
@@ -257,12 +264,11 @@ class UR10Env(gym.Env):
             gym.spaces: Gym observation space object.
 
         """
-
         # Joint position range tolerance
-        pos_tolerance = np.full(6,0.3)
+        pos_tolerance = np.full(6, 0.1)
         # Joint positions range used to determine if there is an error in the sensor readings
-        max_joint_positions = np.add(self.ur10.get_max_joint_positions(), pos_tolerance)
-        min_joint_positions = np.subtract(self.ur10.get_min_joint_positions(), pos_tolerance)
+        max_joint_positions = np.add(np.full(6, 1.0), pos_tolerance)
+        min_joint_positions = np.subtract(np.full(6, -1.0), pos_tolerance)
         # Target coordinates range
         target_range = np.full(3, np.inf)
         # Joint positions range tolerance
@@ -286,18 +292,22 @@ class EndEffectorPositioningUR10(UR10Env):
         # Calculate distance to the target
         target_coord = np.array(rs_state[0:3])
         ee_coord = np.array(rs_state[18:21])
-        euclidean_dist_3d = np.linalg.norm(target_coord-ee_coord)
+        euclidean_dist_3d = np.linalg.norm(target_coord - ee_coord)
 
         # Reward base
-        base_reward = -50*euclidean_dist_3d
-        if self.prev_base_reward is not None:
-            reward = base_reward - self.prev_base_reward
-        self.prev_base_reward = base_reward
+        reward = -1 * euclidean_dist_3d
+        
+        joint_positions = self.ur10._ros_joint_list_to_ur10_joint_list(rs_state[6:12])
+        joint_positions_normalized = self.ur10.normalize_joint_values(joint_positions)
+        delta = np.abs(np.subtract(joint_positions_normalized, action))
+        reward = reward - (0.05 * np.sum(delta))
+
 
         if euclidean_dist_3d <= self.distance_threshold:
             reward = 100
             done = True
             info['final_status'] = 'success'
+            info['target_coord'] = target_coord
 
         # Check if robot is in collision
         if rs_state[25] == 1:
@@ -306,17 +316,53 @@ class EndEffectorPositioningUR10(UR10Env):
             collision = False
 
         if collision:
-            reward = -200
+            reward = -400
             done = True
             info['final_status'] = 'collision'
+            info['target_coord'] = target_coord
 
         if self.elapsed_steps >= self.max_episode_steps:
             done = True
             info['final_status'] = 'max_steps_exceeded'
+            info['target_coord'] = target_coord
 
         return reward, done, info
 
-class EndEffectorPositioningAntiShakeUR10(UR10Env):
+class EndEffectorPositioningUR10DoF5(UR10Env):
+    def __init__(self, rs_address=None, max_episode_steps=300, **kwargs):
+        self.ur10 = ur_utils.UR10()
+        self.max_episode_steps = max_episode_steps
+        self.elapsed_steps = 0
+        self.observation_space = self._get_observation_space()
+        self.action_space = spaces.Box(low=np.full((5), -1.0), high=np.full((5), 1.0), dtype=np.float32)
+        self.seed()
+        self.distance_threshold = 0.1
+        self.abs_joint_pos_range = self.ur10.get_max_joint_positions()
+
+        # Connect to Robot Server
+        if rs_address:
+            self.client = rs_client.Client(rs_address)
+        else:
+            print("WARNING: No IP and Port passed. Simulation will not be started")
+            print("WARNING: Use this only to get environment shape")
+
+
+    def _get_initial_joint_positions(self):
+        """Generate random initial robot joint positions.
+
+        Returns:
+            np.array: Joint positions with standard indexing.
+
+        """
+        # Minimum initial joint positions
+        low = np.array([-0.65, -2.75, 1.0, -3.14, -1.7, 0])
+        # Maximum initial joint positions
+        high = np.array([0.65, -2.0, 2.5, 3.14, 1.7, 0])
+        # Random initial joint positions
+        joint_positions = np.random.default_rng().uniform(low=low, high=high)
+
+        return joint_positions
+
 
     def _reward(self, rs_state, action):
         reward = 0
@@ -326,34 +372,22 @@ class EndEffectorPositioningAntiShakeUR10(UR10Env):
         # Calculate distance to the target
         target_coord = np.array(rs_state[0:3])
         ee_coord = np.array(rs_state[18:21])
-        euclidean_dist_3d = np.linalg.norm(target_coord-ee_coord)
+        euclidean_dist_3d = np.linalg.norm(target_coord - ee_coord)
 
         # Reward base
-        base_reward = - 50*euclidean_dist_3d
-        if self.prev_base_reward is not None:
-            reward = base_reward - self.prev_base_reward
-        self.prev_base_reward = base_reward
+        reward = -1 * euclidean_dist_3d
+        
+        joint_positions = self.ur10._ros_joint_list_to_ur10_joint_list(rs_state[6:12])
+        joint_positions_normalized = self.ur10.normalize_joint_values(joint_positions)
+        delta = np.abs(np.subtract(joint_positions_normalized, action))
+        reward = reward - (0.05 * np.sum(delta))
 
-
-        # Creating punishment for rapid movement
-        penalty = 0
-        if self.last_action is None:
-            self.last_action = action
-        else:
-            for a in range(len(self.last_action)):
-                if abs(self.last_action[a] - action[a]) > 1:
-                    penalty -= 5
-                elif abs(self.last_action[a] - action[a]) > 0.5:
-                    penalty -= 1
-                elif abs(self.last_action[a] - action[a]) > 0.2:
-                    penalty -= 0.2
-            reward += penalty
-            self.last_action = action
 
         if euclidean_dist_3d <= self.distance_threshold:
             reward = 100
             done = True
             info['final_status'] = 'success'
+            info['target_coord'] = target_coord
 
         # Check if robot is in collision
         if rs_state[25] == 1:
@@ -362,20 +396,56 @@ class EndEffectorPositioningAntiShakeUR10(UR10Env):
             collision = False
 
         if collision:
-            reward = -200
+            reward = -400
             done = True
             info['final_status'] = 'collision'
+            info['target_coord'] = target_coord
 
         if self.elapsed_steps >= self.max_episode_steps:
             done = True
             info['final_status'] = 'max_steps_exceeded'
+            info['target_coord'] = target_coord
 
         return reward, done, info
 
+    def step(self, action):
+        self.elapsed_steps += 1
+
+        # Check if the action is within the action space
+        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+        action =  np.append(action, [0.0])
+
+        # Convert environment action to Robot Server action
+        rs_action = copy.deepcopy(action)
+        # Scale action
+        rs_action = np.multiply(rs_action, self.abs_joint_pos_range)
+        # Convert action indexing from ur10 to ros
+        rs_action = self.ur10._ur_10_joint_list_to_ros_joint_list(rs_action)
+        # Send action to Robot Server
+        if not self.client.send_action(rs_action.tolist()):
+            raise RobotServerError("send_action")
+
+        # Get state from Robot Server
+        rs_state = self.client.get_state()
+        # Convert the state from Robot Server format to environment format
+        self.state = self._robot_server_state_to_env_state(rs_state)
+
+        # Check if the environment state is contained in the observation space
+        if not self.observation_space.contains(self.state):
+            raise InvalidStateError()
+
+        # Assign reward
+        reward = 0
+        done = False
+        reward, done, info = self._reward(rs_state=rs_state, action=action)       
+
+        return self.state, reward, done, info
+
+
 class EndEffectorPositioningUR10Sim(EndEffectorPositioningUR10, Simulation):
     cmd = "roslaunch ur_robot_server ur10_sim_robot_server.launch \
-        max_velocity_scale_factor:=0.5 \
-        action_cycle_rate:=25"
+        max_velocity_scale_factor:=0.2 \
+        action_cycle_rate:=10"
     def __init__(self, ip=None, lower_bound_port=None, upper_bound_port=None, gui=False, **kwargs):
         Simulation.__init__(self, self.cmd, ip, lower_bound_port, upper_bound_port, gui, **kwargs)
         EndEffectorPositioningUR10.__init__(self, rs_address=self.robot_server_ip, **kwargs)
@@ -383,13 +453,14 @@ class EndEffectorPositioningUR10Sim(EndEffectorPositioningUR10, Simulation):
 class EndEffectorPositioningUR10Rob(EndEffectorPositioningUR10):
     real_robot = True
 
-class EndEffectorPositioningAntiShakeUR10Sim(EndEffectorPositioningAntiShakeUR10, Simulation):
+class EndEffectorPositioningUR10DoF5Sim(EndEffectorPositioningUR10DoF5, Simulation):
     cmd = "roslaunch ur_robot_server ur10_sim_robot_server.launch \
-        max_velocity_scale_factor:=0.5 \
-        action_cycle_rate:=25"
+        max_velocity_scale_factor:=0.2 \
+        action_cycle_rate:=10"
     def __init__(self, ip=None, lower_bound_port=None, upper_bound_port=None, gui=False, **kwargs):
         Simulation.__init__(self, self.cmd, ip, lower_bound_port, upper_bound_port, gui, **kwargs)
-        EndEffectorPositioningAntiShakeUR10.__init__(self, rs_address=self.robot_server_ip, **kwargs)
+        EndEffectorPositioningUR10DoF5.__init__(self, rs_address=self.robot_server_ip, **kwargs)
 
-class EndEffectorPositioningAntiShakeUR10Rob(EndEffectorPositioningAntiShakeUR10):
+class EndEffectorPositioningUR10DoF5Rob(EndEffectorPositioningUR10DoF5):
     real_robot = True
+    
