2c81af8e873292a7ad7ab2cbf289dcea4962179e
Lucchi, Matteo <matteo.lucchi@joanneum.at>
2020-12-18 17:38:26 +0100

    added n_obstacles to 1 Box spline env

--- robo_gym/envs/ur5/ur5_avoidance.py
+++ robo_gym/envs/ur5/ur5_avoidance.py
@@ -397,7 +397,7 @@ class MovingBox3DSplineTargetUR5(MovingBoxTargetUR5):
         
         string_params = {"function": "3d_spline"}
         float_params = {"x_min": -0.7, "x_max": 0.7, "y_min": 0.2, "y_max": 1.0, "z_min": 0.1, "z_max": 1.0, \
-                        "n_points": 10, "n_sampling_points": n_sampling_points}
+                        "n_points": 10, "n_sampling_points": n_sampling_points, "n_objects":1.0}
         state_msg = robot_server_pb2.State(state = rs_state.tolist(), float_params = float_params, string_params = string_params)
         if not self.client.set_state_msg(state_msg):
             raise RobotServerError("set_state")
