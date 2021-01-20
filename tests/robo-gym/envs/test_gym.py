09541b1d76b24d60a52c6a6afe1c6a8ff064f0e5
Lucchi, Matteo <matteo.lucchi@joanneum.at>
2021-01-20 17:48:29 +0100

    temporarily remnoved some envs from test

--- tests/robo-gym/envs/test_gym.py
+++ tests/robo-gym/envs/test_gym.py
@@ -4,15 +4,15 @@ import pytest
 import robo_gym
 
 envs = [
-    'NoObstacleNavigationMir100Sim-v0', 
-    'ObstacleAvoidanceMir100Sim-v0', 
+    # 'NoObstacleNavigationMir100Sim-v0', 
+    # 'ObstacleAvoidanceMir100Sim-v0', 
     'EndEffectorPositioningUR10Sim-v0', 
-    'EndEffectorPositioningUR10DoF5Sim-v0',
-    'EndEffectorPositioningUR5Sim-v0',
-    'EndEffectorPositioningUR5DoF5Sim-v0',
-    'MovingBoxTargetUR5Sim-v0',
-    'MovingBoxTargetUR5DoF3Sim-v0',
-    'MovingBoxTargetUR5DoF5Sim-v0',
+    # 'EndEffectorPositioningUR10DoF5Sim-v0',
+    # 'EndEffectorPositioningUR5Sim-v0',
+    # 'EndEffectorPositioningUR5DoF5Sim-v0',
+    # 'MovingBoxTargetUR5Sim-v0',
+    # 'MovingBoxTargetUR5DoF3Sim-v0',
+    # 'MovingBoxTargetUR5DoF5Sim-v0',
     'MovingBox3DSplineTargetUR5Sim-v0',
     'MovingBox3DSplineTargetUR5DoF3Sim-v0',
     'ObstacleAvoidance1Box2PointsUR5Sim-v0'
