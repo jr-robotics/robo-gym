b4e341f3443977d97357ce11c74cfc611db8abbf
Matteo Lucchi <matteo.lucchi@joanneum.at>
2021-03-17 16:19:40 +0100

    Revert "updated collision detection test"
    This reverts commit 3454aca751572f16c8c698a044a0707c4e4c06fe.

--- tests/robo-gym/envs/ur/test_collision_detection.py
+++ tests/robo-gym/envs/ur/test_collision_detection.py
@@ -3,29 +3,33 @@ import gym
 import pytest
 
 import robo_gym
-from robo_gym.utils import ur_utils
 
 
 
 
-test_ur_collision = [
-   ('EndEffectorPositioningUR5Sim-v0', [0.0, -1.26, -3.14, 0.0, 0.0, 0.0], 'ur5'), # self-collision
-   ('EndEffectorPositioningUR5Sim-v0', [0.0, 1.0, 1.8, 0.0, 0.0, 0.0], 'ur5'), # collision with ground 
-   ('EndEffectorPositioningUR10Sim-v0', [0.0, -1.5, 3.14, 0.0, 0.0, 0.0], 'ur10'), # self-collision
-   ('EndEffectorPositioningUR10Sim-v0', [0.0, 1.0, 1.15, 0.0, 0.0, 0.0], 'ur10')  # collision with ground 
-
+envs = [
+    'EndEffectorPositioningUR10Sim-v0', 
+    'EndEffectorPositioningUR10DoF5Sim-v0',
+    'EndEffectorPositioningUR5Sim-v0',
 ]
 
 
-@pytest.mark.parametrize('env_name, collision_joint_config, ur_model', test_ur_collision)
-def test_collision_detection(env_name, collision_joint_config, ur_model):
-    ur = ur_utils.UR(model=ur_model)
+@pytest.mark.parametrize('env_name', envs)
+def test_collision_detection(env_name):
     env = gym.make(env_name, ip='robot-servers')
+
     env.reset()
-    action = ur.normalize_joint_values(collision_joint_config)
     done = False
     while not done:
-        _, _, done, info = env.step(action)    
+        action = env.action_space.sample()
+        action = [1 for action in action]
+        _, _, done, info = env.step(action)
+        if done and info['final_status'] == 'success':
+            env.reset()
+            done = False
+            
+
     assert info['final_status'] == 'collision'
+
     env.kill_sim()
     env.close()
