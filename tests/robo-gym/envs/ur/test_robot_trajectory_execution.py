d7848df4df480baafe817003e7804f6458345caf
Matteo Lucchi <matteo.lucchi@joanneum.at>
2021-04-06 18:26:21 +0200

    wip. get joint positions of random step in state 0

--- tests/robo-gym/envs/ur/test_robot_trajectory_execution.py
+++ tests/robo-gym/envs/ur/test_robot_trajectory_execution.py
@@ -1,29 +1,41 @@
 import numpy as np
 import gym
 import pytest
+import pathlib
+import json
 
 import robo_gym
 from robo_gym.utils import ur_utils
 
 
+robo_gym_path = pathlib.Path(__file__).parent.parent.parent.parent.parent.absolute().joinpath('robo_gym')
+
+
 
 
 test_robot_trajectory_iros_params = [
-    ('IrosEnv03UR5TrainingSim-v0', 'ur5'),
-    ('IrosEnv03UR5TestFixedSplinesSim-v0', 'ur5')
+    ('IrosEnv03UR5TrainingSim-v0', 'ur5', 'envs/ur5/robot_trajectories/trajectory_iros_2021.json'),
+    ('IrosEnv03UR5TestFixedSplinesSim-v0', 'ur5', 'envs/ur5/robot_trajectories/trajectory_iros_2021.json')
     ]
 
 
-@pytest.mark.parametrize('env_name, ur_model', test_robot_trajectory_iros_params)
-def test_robot_trajectory_iros(env_name, ur_model):
+@pytest.mark.parametrize('env_name, ur_model, traj_relative_path', test_robot_trajectory_iros_params)
+def test_robot_trajectory_iros(env_name, ur_model, traj_relative_path):
     ur = ur_utils.UR(model=ur_model)
     env = gym.make(env_name, ip='robot-servers')
     env.reset()
-    action = np.zeros(5)
-    joint_positions = [-1.23,-1.80,-1.07,-1.84,1.59, -0.07]
-    for i in range(40):
-        state, _, _, _ = env.step(action)
 
+    # load trajectory 
+    traj_path = robo_gym_path.joinpath(traj_relative_path)
+    with open(traj_path) as json_file:
+        trajectory = json.load(json_file)['trajectory']
+
+    # check step in state 0 
+    step_n = np.random.randint(low = 20, high= len(trajectory[0])) 
+    joint_positions = trajectory[0][step_n]
+    action = np.zeros(5)
+    for i in range(step_n+1):
+        state, _, _, _ = env.step(action)
     assert np.isclose(ur.normalize_joint_values(joint_positions), state[3:9], atol=0.05).all()
 
     env.kill_sim()
