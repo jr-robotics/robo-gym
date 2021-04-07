f886715187763adb5242b6536fc73677b6f88f9e
Matteo Lucchi <matteo.lucchi@joanneum.at>
2021-04-07 19:28:14 +0200

    bug fix

--- robo_gym/wrappers/ur5_avoidance_iros_debug.py
+++ robo_gym/wrappers/ur5_avoidance_iros_debug.py
@@ -1,8 +1,8 @@
 import gym 
+import copy
 
 class UR5AvoidanceIrosDebugWrapper(gym.Wrapper):
-     def _get_desired_joint_positions(self) -> np.array:
-        
+    def _get_desired_joint_positions(self):
         return self.fixed_joint_positions
 
     def reset(self, fixed_joint_positions, **kwargs):
