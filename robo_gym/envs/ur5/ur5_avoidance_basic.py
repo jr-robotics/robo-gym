4af8d3678b0697290ad3cf3b435c717290980da2
Matteo Lucchi <matteo.lucchi@joanneum.at>
2021-04-07 18:31:03 +0200

    bug fix

--- robo_gym/envs/ur5/ur5_avoidance_basic.py
+++ robo_gym/envs/ur5/ur5_avoidance_basic.py
@@ -61,7 +61,7 @@ class MovingBoxTargetUR5(UR5BaseEnv):
         """Environment reset.
 
         Args:
-            initial_joint_positions (list[6] or np.array[6]): robot joint positions in radians.
+            desired_joint_positions (list[6] or np.array[6]): robot joint positions in radians.
 
         Returns:
             np.array: Environment state.
@@ -82,7 +82,7 @@ class MovingBoxTargetUR5(UR5BaseEnv):
         else:
             self._set_desired_joint_positions(DESIRED_JOINT_POSITIONS)
 
-        rs_state[6:12] = self.ur._ur_joint_list_to_ros_joint_list(self.initial_joint_positions)
+        rs_state[6:12] = self.ur._ur_joint_list_to_ros_joint_list(self._get_desired_joint_positions)
 
 
         # Set initial state of the Robot Server
