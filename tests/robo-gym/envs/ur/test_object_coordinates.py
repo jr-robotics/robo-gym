78af519aee8ecd71a1304ca55d3d81e7f6ff92e4
Lucchi, Matteo <matteo.lucchi@joanneum.at>
2021-04-08 18:14:41 +0200

    added test of object coordinates for MovingBoxTargetUR5Sim-v0

--- tests/robo-gym/envs/ur/test_object_coordinates.py
+++ tests/robo-gym/envs/ur/test_object_coordinates.py
@@ -6,7 +6,7 @@ import robo_gym
 from robo_gym.utils import ur_utils
 
 
-test_object_coordinates_params = [
+test_object_coordinates_ee_pos_params = [
    #? robot up-right, target_coord_in_ee_frame 0.0, -0.3, 0.2, coordinates of target calculated using official dimensions from DH parameters. 
    #? first value is d4+d6
    #? second value is: d1+a2+a3+d5
@@ -20,11 +20,27 @@ test_object_coordinates_params = [
 
 ]
 
-@pytest.mark.parametrize('env_name, initial_joint_positions, object_coordinates, polar_coords,  ur_model', test_object_coordinates_params)
-def test_object_coordinates(env_name, initial_joint_positions, object_coordinates, polar_coords, ur_model):
+@pytest.mark.parametrize('env_name, initial_joint_positions, object_coordinates, polar_coords,  ur_model', test_object_coordinates_ee_pos_params)
+def test_object_coordinates_ee_pos(env_name, initial_joint_positions, object_coordinates, polar_coords, ur_model):
    ur = ur_utils.UR(model=ur_model)
    env = gym.make(env_name, ip='robot-servers')
    state = env.reset(initial_joint_positions=initial_joint_positions, ee_target_pose=object_coordinates)
+
+   
+   assert np.isclose([polar_coords['r'], polar_coords['phi'], polar_coords['theta']], state[0:3], atol=0.1).all()
+   
+   env.kill_sim()
+   env.close()
+
+test_object_coordinates_avoidance_basic_params = [
+   ('MovingBoxTargetUR5Sim-v0', [0.0, -1.57, 0.0, -1.57, 0.0, 0.0], [0.0, (-0.191 -0.2), (1.001 + 0.3), 0.0, 0.0, 0.0], {'r': 0.360, 'theta': 0.983, 'phi': -1.571}, 'ur5')  
+]
+
+@pytest.mark.parametrize('env_name, desired_joint_positions, fixed_object_position, polar_coords,  ur_model', test_object_coordinates_avoidance_basic_params)
+def test_object_coordinates_avoidance_basic(env_name, desired_joint_positions, fixed_object_position, polar_coords, ur_model):
+   ur = ur_utils.UR(model=ur_model)
+   env = gym.make(env_name, ip='robot-servers')
+   state = env.reset(desired_joint_positions=desired_joint_positions, fixed_object_position=fixed_object_position)
 
    
    assert np.isclose([polar_coords['r'], polar_coords['phi'], polar_coords['theta']], state[0:3], atol=0.1).all()
