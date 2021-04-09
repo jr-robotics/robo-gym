1367c6abe8d83de3732e9a7d7ec4f9d8e2a3f5a0
Lucchi, Matteo <matteo.lucchi@joanneum.at>
2021-04-09 14:10:26 +0200

    fix syntax error

--- tests/robo-gym/envs/ur/test_object_coordinates.py
+++ tests/robo-gym/envs/ur/test_object_coordinates.py
@@ -49,7 +49,7 @@ def test_object_coordinates_avoidance_basic(env_name, desired_joint_positions, f
    env.close()
 
 test_object_coordinates_avoidance_iros_params = [
-   ('IrosEnv03UR5TrainingSim-v0', [0.3, 0.1, 1.0], {'r': 0.980, 'theta': 2.488, 'phi': 1.246}, {'r': 0.607, 'theta': 1.023, 'phi': -0.825} 'ur5')  
+   ('IrosEnv03UR5TrainingSim-v0', [0.3, 0.1, 1.0], {'r': 0.980, 'theta': 2.488, 'phi': 1.246}, {'r': 0.607, 'theta': 1.023, 'phi': -0.825}, 'ur5')  
 ]
 
 @pytest.mark.parametrize('env_name, fixed_object_position, polar_coords_ee, polar_coords_forearm, ur_model', test_object_coordinates_avoidance_iros_params)
