575b12ed41f9f24b316dcf53ad14284cccde8a2d
Lucchi, Matteo <matteo.lucchi@joanneum.at>
2021-04-09 12:15:53 +0200

    added test of object coords for IrosEnv03UR5TrainingSim-v0

--- tests/robo-gym/envs/ur/test_object_coordinates.py
+++ tests/robo-gym/envs/ur/test_object_coordinates.py
@@ -47,3 +47,20 @@ def test_object_coordinates_avoidance_basic(env_name, desired_joint_positions, f
    
    env.kill_sim()
    env.close()
+
+test_object_coordinates_avoidance_iros_params = [
+   ('IrosEnv03UR5TrainingSim-v0', [0.3, 0.1, 1.0], {'r': 0.980, 'theta': 2.488, 'phi': 1.246}, {'r': 0.607, 'theta': 1.023, 'phi': -0.825} 'ur5')  
+]
+
+@pytest.mark.parametrize('env_name, fixed_object_position, polar_coords_ee, polar_coords_forearm, ur_model', test_object_coordinates_avoidance_iros_params)
+def test_object_coordinates_avoidance_iros(env_name, fixed_object_position, polar_coords_ee, polar_coords_forearm, ur_model):
+   ur = ur_utils.UR(model=ur_model)
+   env = gym.make(env_name, ip='robot-servers')
+   state = env.reset(fixed_object_position=fixed_object_position)
+
+   
+   assert np.isclose([polar_coords_ee['r'], polar_coords_ee['phi'], polar_coords_ee['theta']], state[0:3], atol=0.1).all()
+   assert np.isclose([polar_coords_forearm['r'], polar_coords_forearm['phi'], polar_coords_forearm['theta']], state[22:25], atol=0.1).all()
+   
+   env.kill_sim()
+   env.close()
