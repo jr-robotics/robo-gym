604463f0e834c9f0fc6414e1933124b82d7dce2d
Friedemann Zindler <friedemann.zindler@joanneum.at>
2021-04-20 14:35:02 +0200

    added information to ur base env docstring

--- robo_gym/envs/ur/ur_base_env.py
+++ robo_gym/envs/ur/ur_base_env.py
@@ -15,17 +15,20 @@ from robo_gym.envs.simulation_wrapper import Simulation
 
 JOINT_POSITIONS = [0.0, -2.5, 1.5, 0, -1.4, 0]
 class URBaseEnv(gym.Env):
-    """Universal Robots UR5 base environment.
+    """Universal Robots UR base environment.
 
     Args:
         rs_address (str): Robot Server address. Formatted as 'ip:port'. Defaults to None.
+        fix_base (bool): Wether or not the base joint stays fixed or is moveable. Defaults to False.
+        fix_shoulder (bool): Wether or not the shoulder joint stays fixed or is moveable. Defaults to False.
+        fix_elbow (bool): Wether or not the elbow joint stays fixed or is moveable. Defaults to False.
+        fix_wrist_1 (bool): Wether or not the wrist 1 joint stays fixed or is moveable. Defaults to False.
+        fix_wrist_2 (bool): Wether or not the wrist 2 joint stays fixed or is moveable. Defaults to False.
+        fix_wrist_3 (bool): Wether or not the wrist 3 joint stays fixed or is moveable. Defaults to True.
+        ur_model (str): determines which ur model will be used in the environment. Default to 'ur5'.
 
     Attributes:
         ur (:obj:): Robot utilities object.
-        observation_space (:obj:): Environment observation space.
-        action_space (:obj:): Environment action space.
-        distance_threshold (float): Minimum distance (m) from target to consider it reached.
-        abs_joint_pos_range (np.array): Absolute value of joint positions range`.
         client (:obj:str): Robot Server client.
         real_robot (bool): True if the environment is controlling a real robot.
 
@@ -46,7 +49,6 @@ class URBaseEnv(gym.Env):
 
         self.observation_space = self._get_observation_space()
         self.action_space = self._get_action_space()
-        self.seed()
         self.distance_threshold = 0.1
         self.abs_joint_pos_range = self.ur.get_max_joint_positions()
         self.last_action = None
@@ -58,9 +60,6 @@ class URBaseEnv(gym.Env):
             print("WARNING: No IP and Port passed. Simulation will not be started")
             print("WARNING: Use this only to get environment shape")
 
-    def seed(self, seed=None):
-        self.np_random, seed = gym.utils.seeding.np_random(seed)
-        return [seed]
 
     def _set_initial_robot_server_state(self, rs_state) -> robot_server_pb2.State:
         string_params = {}
