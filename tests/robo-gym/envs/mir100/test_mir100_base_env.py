b473f70528840e037d2f733e34a4760ebe25f10c
Matteo Lucchi <matteo.lucchi@joanneum.at>
2021-05-06 15:36:59 +0200

    added test initialization MiR envs

---
+++ tests/robo-gym/envs/mir100/test_mir100_base_env.py
@@ -0,0 +1,21 @@
+import gym
+import robo_gym
+import pytest
+
+@pytest.fixture(scope='module')
+def env(request):
+    env = gym.make('NoObstacleNavigationMir100Sim-v0', ip='robot-servers')
+    yield env
+    env.kill_sim()
+    env.close()
+
+@pytest.mark.commit 
+def test_initialization(env):
+    env.reset()
+    done = False
+    for _ in range(10):
+        if not done:
+            action = env.action_space.sample()
+            observation, _, done, _ = env.step(action)
+
+    assert env.observation_space.contains(observation)
