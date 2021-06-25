1c959c7a93312561ebc746988ed144f9a46d9e0c
Matteo Lucchi <matteo.lucchi@joanneum.at>
2021-06-25 11:34:17 +0200

    added example environment

--- robo_gym/__init__.py
+++ robo_gym/__init__.py
@@ -2,6 +2,17 @@ from gym.envs.registration import register
 
 # naming convention: EnvnameRobotSim
 
+# Example Environments
+register(
+    id='ExampleEnvSim-v0',
+    entry_point='robo_gym.envs:ExampleEnvSim',
+)
+
+register(
+    id='ExampleEnvRob-v0',
+    entry_point='robo_gym.envs:ExampleEnvRob',
+)
+
 # MiR100 Environments
 register(
     id='NoObstacleNavigationMir100Sim-v0',
