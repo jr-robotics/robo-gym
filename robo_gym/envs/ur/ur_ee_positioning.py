2093f20ce33b280f1e9a8ce7a052a1a7e2e307d6
Matteo Lucchi <matteo.lucchi@joanneum.at>
2021-07-26 16:40:05 +0200

    add rviz config

--- robo_gym/envs/ur/ur_ee_positioning.py
+++ robo_gym/envs/ur/ur_ee_positioning.py
@@ -370,4 +370,4 @@ class EndEffectorPositioningURSim(EndEffectorPositioningUR, Simulation):
 class EndEffectorPositioningURRob(EndEffectorPositioningUR):
     real_robot = True
 
-# roslaunch ur_robot_server ur_robot_server.launch ur_model:=ur5 real_robot:=true rviz_gui:=true gui:=true reference_frame:=base_link max_velocity_scale_factor:=0.1 action_cycle_rate:=10 objects_controller:=true rs_mode:=1object n_objects:=1.0 object_0_frame:=target
+# roslaunch ur_robot_server ur_robot_server.launch ur_model:=ur5 real_robot:=true rviz_gui:=true rviz_config_file:=ur_rob_with_target_tf_rl.rviz  gui:=true reference_frame:=base_link max_velocity_scale_factor:=0.1 action_cycle_rate:=10 objects_controller:=true rs_mode:=1object n_objects:=1.0 object_0_frame:=target
