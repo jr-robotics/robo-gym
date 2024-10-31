7dbc6d009fd65e6a6f42c191c77bd2423174e42f
Mvukadinovic123 <67293711+Mvukadinovic123@users.noreply.github.com>
2024-10-31 11:08:36 +0100

    Add files via upload
    panda random agent example

---
+++ docs/examples/random_agent_sim_panda.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+import robo_gym
+from robo_gym.wrappers.exception_handling import ExceptionHandling
+
+target_machine_ip = '127.0.0.1' # or other machine 'xxx.xxx.xxx.xxx'
+
+# initialize environment
+env = gym.make('EndEffectorPositioningPandaSim-v0', ip=target_machine_ip, gui=True)
+env = ExceptionHandling(env)
+
+num_episodes = 10
+
+for episode in range(num_episodes):
+    done = False
+    env.reset()
+    while not done:
+        # random step in the environment
+        state, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        done = terminated or truncated
