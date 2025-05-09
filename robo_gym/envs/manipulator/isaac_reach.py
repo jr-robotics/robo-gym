fix: reach rotation due to quaternion order in Isaac Reach observation
could greatly reduce tolerance required for success

+ some test code on RPY to quaternion conversion vs. Isaac

robo_gym/envs/manipulator/isaac_reach.py
@@ -20,10 +20,10 @@ class IsaacReachEnv(ManipulatorEePosEnv):
 
         RoboGymEnv.set_default(kwargs, ManipulatorEePosEnv.KW_EE_ROTATION_MATTERS, True)
         RoboGymEnv.set_default(
-            kwargs, ManipulatorEePosEnv.KW_EE_ROTATION_THRESHOLD, 0.4
+            kwargs, ManipulatorEePosEnv.KW_EE_ROTATION_THRESHOLD, 0.05
         )
         RoboGymEnv.set_default(
-            kwargs, ManipulatorEePosEnv.KW_EE_DISTANCE_THRESHOLD, 0.03
+            kwargs, ManipulatorEePosEnv.KW_EE_DISTANCE_THRESHOLD, 0.02
         )
 
         RoboGymEnv.set_default(
@@ -114,7 +114,7 @@ class IsaacReachObservationNode(ManipulatorEePosObservationNode):
         joint_velocities = self.extract_joint_velocities_from_rs_state_dict(
             rs_state_dict
         )
-        command = np.array(self.get_target_pose())
+        command = utils.pose_quat_wxyz_from_xyzw(np.array(self.get_target_pose()))
         obs = np.concatenate((joint_positions, joint_velocities, command))
         return obs
 

robo_gym/utils/utils.py
@@ -190,7 +190,7 @@ def quat_from_euler_zyx(
     return quat_from_euler(roll, pitch, yaw, "zyx", quat_unique)
 
 
-def euler_from_quat(q: NDArray, seq="zyz") -> NDArray:
+def euler_from_quat(q: NDArray, seq="xyz") -> NDArray:
     rot = R.from_quat(q)
     rpy = rot.as_euler(seq=seq, degrees=False)
     return rpy
@@ -255,7 +255,7 @@ def euler_xyz_from_quat_isaac(
     sin_pitch = 2.0 * (q_w * q_y - q_z * q_x)
     pitch = (
         math.copysign(math.pi / 2.0, sin_pitch)
-        if math.abs(sin_pitch) >= 1
+        if abs(sin_pitch) >= 1
         else math.asin(sin_pitch)
     )
 
@@ -320,9 +320,11 @@ def create_random_bounding_box_pose_quat(
     pitch = get_uniform_from_range(np_random, pitch_range, 0.0)
     yaw = get_uniform_from_range(np_random, yaw_range, 0.0)
 
-    quat = quat_from_euler(roll, pitch, yaw, seq)
+    # quat = quat_from_euler(roll, pitch, yaw, seq)
+    quat = quat_from_euler_xyz_isaac(roll, pitch, yaw)
     if quat_unique and quat[3] < 0:
         quat = -quat
+
     result = np.array([x, y, z, quat[0], quat[1], quat[2], quat[3]])
     return result
 
@@ -351,3 +353,30 @@ def rotation_error_magnitude(q1: NDArray, q2: NDArray) -> float:
     r2 = R.from_quat(q2)
     r_diff = r1 * (r2.inv())
     return r_diff.magnitude()
+
+
+# our representation: xyzw
+# Isaac representation: wxyz (not in the Isaac methods here, they are already converted)
+# but important for providing Isaac-like observations
+def quat_xyzw_from_wxyz(q: NDArray) -> NDArray:
+    # w = q[0]
+    return np.array([q[1], q[2], q[3], q[0]])
+
+
+def quat_wxyz_from_xyzw(q: NDArray) -> NDArray:
+    # w = q[3]
+    return np.array([q[3], q[0], q[1], q[2]])
+
+
+def pose_quat_xyzw_from_wxyz(pose: NDArray) -> NDArray:
+    pos = pose[0:3]
+    q = pose[3:7]
+    result = np.concatenate((pos, quat_xyzw_from_wxyz(q)))
+    return result
+
+
+def pose_quat_wxyz_from_xyzw(pose: NDArray) -> NDArray:
+    pos = pose[0:3]
+    q = pose[3:7]
+    result = np.concatenate((pos, quat_wxyz_from_xyzw(q)))
+    return result

tests/robo-gym/utils/test_utils.py
@@ -223,11 +223,73 @@ def test_quat_to_rpy():
     # - 3.14
     # seq = "zyz" looks right but doesn't make sense?
     # seq = "yxz"
-    seq = "xyz"
-    rpy1 = utils.euler_from_quat(np.array(quat1), seq)
-    rpy2 = utils.euler_from_quat(np.array(quat2), seq)
-    rpy3 = utils.euler_from_quat(np.array(quat3), seq)
-    rpy4 = utils.euler_from_quat(np.array(quat4), seq)
+    results = {}
+    for seq in ["zyz", "xyz", "xzy", "yxz", "yzx", "zyx", "zyx"]:
+        seq_result = []
+        # for quat in [quat1, quat2, quat3, quat4]:
+        for quat in [quat1]:
+            rpy2 = utils.euler_from_quat(np.array(quat), seq)
+            quat2 = utils.quat_from_euler(rpy2[0], rpy2[1], rpy2[2], seq, True)
+            rpy = utils.euler_from_quat(np.array(quat2), seq)
+
+            variant_result = math.isclose(rpy[0], 0, abs_tol=0.0001) and math.isclose(
+                rpy[1], math.pi / 2, abs_tol=0.0001
+            )
+            seq_result.append(variant_result)
+        results[seq] = seq_result
+    foo = "bar"
+
+
+def test_quat_to_rpy_isaac():
+    # side purpose is to evaluate whether we agree with Isaac on the meaning of rpy or if we need to adapt
+
+    # Isaac quaternions, already changed to our usual order xyzw:
+    quat1 = [
+        -0.138306,
+        0.693359,
+        0.138306,
+        0.693359,
+    ]
+    quat2 = [
+        0.588379,
+        0.392334,
+        -0.588379,
+        0.392334,
+    ]
+    quat3 = [
+        0.634277,
+        0.3125,
+        -0.634277,
+        0.3125,
+    ]
+    quat4 = [
+        -0.121277,
+        0.696777,
+        0.121277,
+        0.696777,
+    ]
+    # They all come from such a range configuration:
+    # roll: !!python / tuple
+    # - 0.0
+    # - 0.0
+    # pitch: !!python / tuple
+    # - 1.5707963267948966
+    # - 1.5707963267948966
+    # yaw: !!python / tuple
+    # - -3.14
+    # - 3.14
+    # seq = "zyz" looks right but doesn't make sense?
+    # seq = "yxz"
+    results = {}
+    for seq in ["isaac"]:
+        seq_result = []
+        for quat in [quat1, quat2, quat3, quat4]:
+            rpy = utils.euler_xyz_from_quat_isaac(np.array(quat))
+            variant_result = math.isclose(rpy[0], 0, abs_tol=0.0001) and math.isclose(
+                rpy[1], math.pi / 2, abs_tol=0.0001
+            )
+            seq_result.append(variant_result)
+        results[seq] = seq_result
     foo = "bar"
 
 
@@ -236,6 +298,28 @@ def test_quat_from_rpy2():
     q2 = utils.quat_from_euler_xyz(math.pi, 0, 0)
     q12 = q1 * q2
     quat = [q12[0], q12[1], q12[2], q12[3]]
+
+
+def test_quat_from_rpy_vs_isaac():
+    rpy_isaac = (0, math.pi / 2, 0.3495)
+    quat_isaac = utils.quat_from_euler_xyz_isaac(
+        rpy_isaac[0], rpy_isaac[1], rpy_isaac[2]
+    )
+    quat = utils.quat_from_euler_xyz(rpy_isaac[0], rpy_isaac[1], rpy_isaac[2], False)
+    # they are equal, so far so good
+
+    # this is not equal to what we started with:
+    rpy_back_isaac = utils.euler_xyz_from_quat_isaac(quat)
+
+    # zyz gives us consistent rpys for the values we tried from Isaac so far,
+    # but it may be special cases
+    results = {}
+    for seq in ["zyz", "xyz", "xzy", "yxz", "yzx", "zyx", "zyx"]:
+
+        rpy_back = utils.euler_from_quat(quat, seq)
+        results[seq] = rpy_back
+
+    foo = "bar"
 
 
 def test_rot_diff():